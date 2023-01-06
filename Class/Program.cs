a817f5b480673af1301e3405910d2d8b19e09e70
Sarugen <sepenzev97@gmail.com>
2023-01-06 19:07:48 +0200

    Add project files.

---
+++ Class/Program.cs
@@ -0,0 +1,34 @@
+﻿var house1 = new House()
+{
+  
+};
+
+
+class House
+{
+    public readonly string Address = "Street";
+
+    public House()
+    {
+        if (Address.Length >= 5)
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+
+    public override string ToString()
+    {
+        if (house1.name ==! null)
+        {
+            Console.WriteLine("${house.name}");
+        }
+        else
+        {
+            Console.WriteLine("${Address}");
+        }
+    }
+};
